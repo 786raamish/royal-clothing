94bf56e15a17f7203695f79f07f569b4ad21a234
Ramish <786alam.raamish@gmail.com>
2020-02-26 23:37:34 +0500

    Broken our homepage into smaller components and finished styling our menu item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+
+
+import './homepage.styles.scss';
+
+
+const HomePage = () => (
+    // eslint-disable-next-line no-unused-expressions
+    <div className = 'homapage'>
+        <Directory/>
+    </div>
+);
+
+export default HomePage;
